69ae423a4c5f6b75c7fbf53edf19f1957fa41186
Farid324 <geelgaga@hotmail.com>
2022-07-04 20:34:10 -0400

    Variables

---
+++ JavaScript/clase1.js
@@ -0,0 +1,42 @@
+alert("Hola Mundo Elemental");
+//Hola soy un comentario
+/*HOla para parrafos largos*/
+
+//Valores y tipos de datos
+//Valores primitivos
+typeof 40;
+typeof "Elemental";
+typeof true;
+typeof null;
+typeof ["Manzana", 23, true, "c"];
+
+//Tipos de datos
+
+var x = "Elemental";
+
+console.log("Hola "+ x);
+
+//Utilidad de las variables
+console.log("El perimetro de un cuadrado de lado 5 es"+(5*4));
+
+var lado = 20;
+
+console.log("El perimetro de un cuadrado de lado 5 es"+(lado*4));
+
+var nombre;
+
+//Tipos de Variables
+
+//var
+//let
+//const para mas seguridad
+
+const password = "1234567";
+console.log("password"+password);
+
+//Operadores matematicos
+
+/*+ Suma, Concatenar
+++ Incremento
+- Sustracción
+-- */
